1aee004a02dd7270dfff56045bd8ba8309bc7f98
maritzaalamo <39917899+maritzaalamo@users.noreply.github.com>
2020-03-10 09:03:41 -0400

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,25 @@
+function loadBooks(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var book = this.responseText;
+      var booksObj = JSON.parse(books);
+      for (x = 0; x < booksObj.books.length; x++)
+      {
+        showBooks(booksObj, x);
+      }
+    }
+  };
+  xhttp.open("GET","books.json",true);
+  xhttp.send();
+}
+
+function showBooks(booksObj, x){
+  var book = booksObj.books[x];
+  var titleID = "book" + x + "Title";
+  var summaryID = "book" + x + "Summary";
+  var coverID = "book" + x + "Cover";
+  document.getElementById(titleID).innerHTML = book.title;
+  document.getElementById(summaryID).innerHTML = book.summary;
+  document.getElementById(coverID).src = book.coverImage;
+}
